e6af44502fa3dcd08d07231c1e3e1979332a820c
Viky <pilonevictoria@gmail.com>
2024-08-21 20:59:06 -0300

    Update de manejadores segun indicaciones del profe

---
+++ handlers/ManejadorDonacion.java
@@ -0,0 +1,27 @@
+package handlers;
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Donacion;
+
+public class ManejadorDonacion {
+	private static ManejadorDonacion instancia = null;
+	private List<Donacion> donaciones = new ArrayList<Donacion>();
+	
+	private ManejadorDonacion() {}
+
+	public static ManejadorDonacion getInstancia() {
+		if (instancia == null) 
+		{
+			instancia = new ManejadorDonacion();
+		}
+		return instancia;
+	}
+	
+	public void añadirDonacion(Donacion don) {
+		this.donaciones.add(don);
+	}
+	
+	
+	
+}
---
+++ handlers/ManejadorUsuario.java
@@ -0,0 +1,27 @@
+package handlers;
+import java.util.ArrayList;
+import java.util.List;
+
+import classes.Usuario;
+
+public class ManejadorUsuario {
+	private static ManejadorUsuario instancia = null;
+	private List<Usuario> usuarios = new ArrayList<Usuario>();
+	
+	private ManejadorUsuario() {}
+
+	public static ManejadorUsuario getInstancia() {
+		if (instancia == null) 
+		{
+			instancia = new ManejadorUsuario();
+		}
+		return instancia;
+	}
+	
+	public void añadirUsuario(Usuario user) {
+		this.usuarios.add(user);
+	}
+	
+	
+	
+}
