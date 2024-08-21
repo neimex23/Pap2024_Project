baf097e321bde84b8bed3994623c48ea1c3d91e8
Neimex23 <ezequielmedina23@gmail.com>
2024-08-20 21:39:35 -0300

    Añadido interfaz y controlador

---
+++ handlers/Controlador.java
@@ -0,0 +1,19 @@
+package handlers;
+
+import dtClasses.DtFechaHora;
+
+public class Controlador implements IControlador {
+
+    public Controlador() {super();}
+
+    @Override
+    public void altaBeneficiario(string name, String email, String adr, DtFechaHora bD, Estado benState, Bario barrio){
+
+    }
+
+    @Override
+    public void altaRepartidor(string name, String email, String driverLicense) {
+
+    }
+
+
