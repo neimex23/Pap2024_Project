3b1f45c562727d4582b4c7c1c22d0c9c31d72cb6
Neimex23 <ezequielmedina23@gmail.com>
2024-10-08 20:24:56 -0300

    Inicializacion de publicadores

---
+++ src/main/java/org/pap/publicadores/ControladorPublish.java
@@ -0,0 +1,41 @@
+package org.pap.publicadores;
+
+import org.pap.interfaces.*;
+import org.pap.dtClasses.*;
+import org.pap.configuraciones.*;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
+import jakarta.jws.soap.SOAPBinding.ParameterStyle;
+import jakarta.jws.soap.SOAPBinding.Style;
+import javax.xml.ws.Endpoint;
+
+public class ControladorPublish {
+    private Fabrica fabrica;
+    private IControlador icon;
+    private WebServiceConfiguracion configuracion;
+    private Endpoint endpoint;
+
+    public ControladorPublish() {
+        fabrica = Fabrica.getInstancia();
+        icon = fabrica.getIControlador();
+        try {
+            configuracion = new WebServiceConfiguracion();
+        } catch (Exception ex) {
+
+        }
+    }
+
+    @WebMethod(exclude = true)
+    public void publicar() {
+        endpoint = Endpoint.publish("http://" + configuracion.getConfigOf("#WS_IP") + ":" + configuracion.getConfigOf("#WS_PORT") + "/controlador", this);
+        System.out.println("http://" + configuracion.getConfigOf("#WS_IP") + ":" + configuracion.getConfigOf("#WS_PORT") + "/controlador");
+    }
+
+    @WebMethod(exclude = true)
+    public Endpoint getEndpoint() {
+        return endpoint;
+    }
+
+}
